3a85b815999abb7eca61aea8a8e08919899b6ede
gl-ward <glward@linkedin.com>
2022-09-12 17:58:47 +0100

    connection string updated

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-    mongoDbUrl: 'mongodb://localhost:27017/mots-cms'
+    //mongoDbUrl: 'mongodb://localhost:27017/mots-cms' mongodb+srv://cms-user:<password>@mots-cms.h8ioynx.mongodb.net/test
+    mongoDbUrl: 'mongodb+srv://cms-user:qwer1234@mots-cms.h8ioynx.mongodb.net/test'
 
 };
